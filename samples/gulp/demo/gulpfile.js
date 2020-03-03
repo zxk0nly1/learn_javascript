f49cdbb4c072125826e72dac6350dce30b46feeb
zxk <1272667023@qq.com>
2020-03-03 21:01:44 +0800

    finshed the gulp project

---
+++ samples/gulp/demo/gulpfile.js
@@ -0,0 +1,5 @@
+function defaultTask(cb) {
+    console.log('gulp启动成功');
+    cb();
+}
+exports.default = defaultTask;
---
+++ samples/gulp/my-project/src/js/index.js
@@ -0,0 +1,2 @@
+var helloworld = document.querySelector('#app');
+helloworld.innerHTML = "hello world , 你好世界"
